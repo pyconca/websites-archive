7a7a5249c0b54497c582f60fc68f85cd85606fcb
Myles Braithwaite <me@mylesbraithwaite.com>
2016-05-17 11:20:03 -0400

    Adding a simple Fabric script to deploy the files.

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import task, env
+from fabric.contrib.project import rsync_project
+
+env.user = 'deploy'
+env.hosts = ['portland.pynorth.org']
+env.use_ssh_config = True
+
+# Remote Directories
+env.html_dir = '/srv/www/pycon.ca/{0}/html/'
+env.archives = ['2012', '2013', '2015']
+
+@task
+def deploy():
+    for archive in env.archives:
+        rsync_project(remote_dir=env.html_dir.format(archive),
+                      local_dir='./{0}.pycon.ca/'.format(archive))
